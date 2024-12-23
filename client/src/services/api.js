4bbf35166c397d9a9e6b0fc0e5fde9c0f1de4678
Shohruz <shokhruzisroilov@gmail.com>
2024-12-23 19:11:21 +0500

    add api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-	baseURL: 'http://localhost:5000/api',
+	baseURL: 'https://farrukhbek-article-website-yiz8.vercel.app/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
