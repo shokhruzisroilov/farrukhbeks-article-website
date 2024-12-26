9cef3ffeabffbbfb7795bc86b50a9c149c5d8082
Shohruz <shokhruzisroilov@gmail.com>
2024-12-26 21:31:43 +0500

    api url change

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-	// baseURL: 'https://farrukhbek-article-website-yiz8.vercel.app/api',
-	baseURL: 'http://localhost:5000/api',
+	baseURL: 'https://farrukhbek-article-website-yiz8.vercel.app/api',
+	// baseURL: 'http://localhost:5000/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
