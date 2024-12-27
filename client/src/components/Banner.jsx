968de6e53286280f44ecf6b3ad0dad0153f6bb5f
Shohruz <shokhruzisroilov@gmail.com>
2024-12-27 22:56:41 +0500

    Banner change

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -3,7 +3,7 @@ import { FaArrowRight } from 'react-icons/fa6'
 
 const Banner = () => {
 	return (
-		<div className='px-6 py-36 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 m-auto'>
+		<div className='px-6 py-36 bg-gradient-to-r from-[#0F172A] via-[#1E40AF] to-[#22C55E] m-auto'>
 			<div className='text-white text-center'>
 				<h1 className='text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-6'>
 					Iqtisodiyot Sohasidagi Eng Yangi Ma'lumotlar
@@ -15,8 +15,8 @@ const Banner = () => {
 				</p>
 				<div className='flex justify-center'>
 					<Link
-						to='/articles' // Maqolalar bo'limiga yo'naltiradi
-						className='bg-orange-500 text-white font-semibold py-3 px-6 rounded-lg text-lg inline-flex items-center hover:bg-orange-600 transition-colors duration-300'
+						to='/articles'
+						className='text-[#FFD700] font-semibold text-lg inline-flex items-center hover:text-[#FFC107] transition-colors duration-300'
 					>
 						Ko'proq bilish <FaArrowRight className='mt-1 ml-2' />
 					</Link>
